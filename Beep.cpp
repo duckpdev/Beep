011833b0de6b77419daddff74019ac2521d31759
Duck.dev <94231866+Duck-Dev228@users.noreply.github.com>
2024-03-25 11:27:50 +0300

    Add files via upload

---
+++ Beep.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <windows.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Frequency is null" << std::endl;
+        return 1;
+    }
+    DWORD frequency = atoi(argv[1]);
+    DWORD duration = 200;
+    if (argc >= 3) {
+        duration = atoi(argv[2]);
+    }
+    Beep(frequency, duration);
+    return 0;
+}
